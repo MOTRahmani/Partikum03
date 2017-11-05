b0680f1370592e8809af8fd707a442ebba70c7bc
MOTRahmani <muhammadrahmani84@gmail.com>
2017-11-05 14:02:12 +0800

    Menambahkan percabangan yang sama, hanya beda diwaktu
    Menambahkan percabangan yang sama, hanya beda diwaktu

--- Untitled3.cpp
+++ Untitled3.cpp
@@ -12,4 +12,20 @@ int main(){
 	}else{
 		gajiTotal[0]=gajiPerminggu*0.85;
 	}
+	cout << "waktu anda bekerja minggu ke-2 = "; cin >> waktu[1];
+	if (waktu[1]>40){
+		gajiLembur=(waktu[1]-40)*1.5*gajiJam;
+		gajiTotal[1]=(gajiPerminggu+gajiLembur)*0.85;
+		gajiAwal=gajiPerminggu*0.85;
+	}else{
+		gajiTotal[1]=gajiPerminggu*0.85;
+	}
+	cout << "waktu anda bekerja minggu ke-3 = "; cin >> waktu[2];
+	if (waktu[2]>40){
+		gajiLembur=(waktu[2]-40)*1.5*gajiJam;
+		gajiTotal[2]=(gajiPerminggu+gajiLembur)*0.85;
+		gajiAwal=gajiPerminggu*0.85;
+	}else{
+		gajiTotal[2]=gajiPerminggu*0.85;
+	}
 }
