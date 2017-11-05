b9b3a69d87488a652121189fe85e7936ad7e1d18
MOTRahmani <muhammadrahmani84@gmail.com>
2017-11-05 14:41:59 +0800

    Inputkan tinggi dan berat badan
    Inputkan tinggi dan berat badan

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 
 int main(){
-	
+	int tinggi.berat;
+	cout << "~~~ PROGRAM TES BADAN ~~~" << endl;
+	cout << "Input Berapa Tinggi badan Kamu = "; cin >> tinggi;
+	cout << "Input Berapa Berat badan kamu = "; cin >> berat;
 }
